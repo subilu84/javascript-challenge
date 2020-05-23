3bc9a17c12c94798e0bf5366777fda2157c5e209
Suellen Maltz <60326149+subilu84@users.noreply.github.com>
2020-05-23 16:32:26 -0400

    Add files via upload

---
+++ level1/static1/js/app.js
@@ -0,0 +1,43 @@
+// from data.js
+var tableData = data;
+
+
+// Insert the data in the table
+var tbody = d3.select("tbody");
+console.log(tableData);
+
+data.forEach((UFO) => {
+    var row = tbody.append("tr");
+    Object.entries(UFO).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  })
+
+// Make the button act
+
+var button = d3.select("#filter-btn");
+button.on("click", function () {
+  tbody.html("");
+  var inputElement = d3.select("#datetime");
+  
+  var inputValue = inputElement.property("value");
+  
+  console.log(inputValue);
+  
+  var filteredData = tableData.filter(result => result.datetime === inputValue);
+
+  console.log(filteredData);
+
+
+  filteredData.forEach((report) => {
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+  });
+});
+ 
